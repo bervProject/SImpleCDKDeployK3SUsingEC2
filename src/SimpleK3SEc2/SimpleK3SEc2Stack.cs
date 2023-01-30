38bb835366c0c70fc21c4f227fb2f6d53c06f410
Bervianto Leo Pratama <bervianto.leo@gmail.com>
2023-01-30 20:20:25 +0700

    Initial commit

---
+++ src/SimpleK3SEc2/SimpleK3SEc2Stack.cs
@@ -0,0 +1,13 @@
+using Amazon.CDK;
+using Constructs;
+
+namespace SimpleK3SEc2
+{
+    public class SimpleK3SEc2Stack : Stack
+    {
+        internal SimpleK3SEc2Stack(Construct scope, string id, IStackProps props = null) : base(scope, id, props)
+        {
+            // The code that defines your stack goes here
+        }
+    }
+}
